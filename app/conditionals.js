6cb31880b8c2a1892e971cc84c836e41ff143708
Stephanie Hooker <stephanieh799@gmail.com>
2019-08-13 22:00:44 -0600

    completed js homework

--- app/conditionals.js
+++ app/conditionals.js
@@ -2,7 +2,11 @@
 
 function largerNum(num1, num2) {
     // you code here
-
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,6 +18,20 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
+    let percentage = score / possible * 100;
+    if (percentage >= 90) {
+        return "A";
+    } else if (percentage >= 80 && percentage < 90) {
+        return "B";
+    } else if (percentage >= 70 && percentage < 80) {
+        return "C";
+    } else if (percentage >= 60 && percentage < 70) {
+        return "D";
+    } else if (percentage >= 50 && percentage < 60) {
+        return "F";
+    } else {
+        return "F";
+    }
 
 }
 
@@ -21,7 +39,8 @@ function testGrader(score, possible) {
 // --------------------------------------------
 
 
-// 3. Given an integer that represents an hour in the day (1 - 24), write a function that returns "good morning", "good afternoon", "good evening", or "good night" based off of what hour it is. You'll need to think in military time.
+// 3. Given an integer that represents an hour in the day (1 - 24), write a function that returns 
+//"good morning", "good afternoon", "good evening", or "good night" based off of what hour it is.You'll need to think in military time.
 //Morning is between 5am and 11am (5 - 11)
 //afternoon is between 12pm and 5pm (12 - 17)
 //evening is between 6pm and 9pm (18 - 21)
@@ -30,12 +49,28 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
-
+    if (hour >= 5 && hour <= 11) {
+        return "good morning";
+    } else if (hour >= 12 && hour <= 17) {
+        return "good afternoon";
+    } else if (hour >= 18 && hour <= 21) {
+        return "good evening";
+    } else if ((hour >= 22 && hour <= 24) || (hour >= 1 && hour <= 4)) {
+        return "good night";
+    }
 }
 
-//4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
+//4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and 
+//additionally if the person should go to the hospital(at or above 103) 'fever go to hospital'
+//(hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+    if (temp <= 98.6) {
+        return "no fever";
+    } else if (temp > 98.6 && temp < 103) {
+        return "fever";
+    } else {
+        return "fever go to hospital";
+    }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +82,11 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    if (car.moving === true) {
+        return false;
+    } else if (car.moving === false) {
+        return true;
+    }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +97,9 @@ let dish = {
 }
 
 function washDish(dish) {
-
+    if (dish.yourDish === true && dish.isDirty === true) {
+        return true;
+    } else {
+        return false;
+    }
 }
--- app/switches.js
+++ app/switches.js
@@ -1,13 +1,46 @@
 // 1. Given a string with the value of a day of the week, return the number that that day is in the week. A second argument will be provided to determine if should start week on Monday if true, else Sunday if false. If the string is not a day of the week but is bad input, then return the string 'That's not a day of the week'.
 // Example: 
-    // input: 'Sunday', false
-    // output: 1
+// input: 'Sunday', false
+// output: 1
 //Example:
-    // input: 'Sunday', true
-    // output: 7
+// input: 'Sunday', true
+// output: 7
 
 function daysPosition(day, offset) {
 
+    let dayOfWeek;
+    switch (day) {
+        case "sunday":
+            dayOfWeek = 1;
+            if (offset === true) {
+                dayOfWeek = 7;
+            }
+            break;
+        case "monday":
+            dayOfWeek = 2;
+            if (offset === true) {
+                dayOfWeek = 1;
+            }
+            break;
+        case "tuesday":
+            dayOfWeek = 3;
+            break;
+        case "wednesday":
+            dayOfWeek = 4;
+            break;
+        case "thursday":
+            dayOfWeek = 5;
+            break;
+        case "friday":
+            dayOfWeek = 6;
+            break;
+        case "saturday":
+            dayOfWeek = 7;
+            break;
+        default:
+            dayOfWeek = "That\'s not a day of the week";
+    }
+    return dayOfWeek;
 }
 
 
@@ -26,16 +59,41 @@ function daysPosition(day, offset) {
  *            >= +3     |   "Ouch"
  */
 
- function golfScore(score, par) {
+function golfScore(score, par) {
+    let term;
+    switch (score - par) {
+        case -3:
+            term = "Ace";
+            break;
+        case -2:
+            term = "Eagle";
+            break;
+        case -1:
+            term = "Birdie";
+            break;
+        case 0:
+            term = "Par";
+            break;
+        case 1:
+            term = "Bogie";
+            break;
+        case 2:
+            term = "Double Bogie";
+            break;
+        default:
+            term = "Ouch";
+    }
+    return term;
+}
 
- }
- 
 
 // --------------------------------------------
 
 
-// 3. Counting Cards. In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called card counting.  
-// Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+// 3. Counting Cards. In the casino game Blackjack, a player can gain an advantage over the house by keeping track 
+// of the relative number of high and low cards remaining in the deck.This is called card counting.  
+// Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the
+// table below.When the count is positive, the player should bet high.When the count is zero or negative, the player should bet low.
 /**
  *      Value   |   Cards
  *   --------------------------------
@@ -43,14 +101,40 @@ function daysPosition(day, offset) {
  *        0     |	'7', '8', '9' 
  *       -1	    |   '10', 'J', 'Q', 'K','A' 
  */
-// Write a card counting function that will receive a card. The function will increment or decrement the global count variable according to the card's value (see table above). The function will then return the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative.
+// Write a card counting function that will receive a card. The function will increment or decrement the global 
+// count variable according to the card's value (see table above). The function will then return the current count 
+// and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative.
 // Example:
-    // output: '-5 Hold'
+// output: '-5 Hold'
 // Example:
-    // output: '2 Bet'
+// output: '2 Bet'
 
 let count = 0
 
 function cardCounter(card) {
+    switch (card) {
 
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+            count++;
+            break;
+        case "7":
+        case "8":
+        case "9":
+            break;
+        case "10":
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+    }
+    if (count > 0) {
+        return count + " Bet";
+    } else {
+        return count + " Hold";
+    }
 }
